b05a865e7177f92f8a2e3727b219774b58c6c127
Carlos Zabaleta <zabaletacopa@gmail.com>
2018-01-03 00:27:03 -0500

    Find parent_id and child_id for each node

--- python_file.py
+++ python_file.py
@@ -1,10 +1,19 @@
 import urllib2
 import json
 
-url = "https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1"
 
-json_obj = urllib2.urlopen(url)
-data = json.load(json_obj)
+def show_pages(number):
+    url = "https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1&page=" + number
+    json_obj = urllib2.urlopen(url)
+    data = json.load(json_obj)
+    for each in (data['menus']):
+        if "parent_id" in each:
+            print 'id-' + str(each["id"]), 'parent-' + str(each["parent_id"]), 'child' + str(each["child_ids"])
+        else:
+            print "id-" + str(each['id']), "child-" + str(each['child_ids'])
 
-for each in (data['menus']):
-    print(str(each["id"]) + " " + str(each["child_ids"]))
+
+x = 1
+while x < 5:
+    show_pages(str(x))
+    x = x + 1

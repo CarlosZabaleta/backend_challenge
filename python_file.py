f892cf51b72aea1916d987da08eb571af872334a
Carlos Zabaleta <zabaletacopa@gmail.com>
2018-01-11 20:48:53 -0500

    Fix id_root problem

--- python_file.py
+++ python_file.py
@@ -13,8 +13,8 @@ import json
 # Add changeable params to the URL
 
 def get_url(num_page, id_number):
-    url = "https://backend-challenge-summer-2018.herokuapp.com/challenges.json"
-    params = {"id": id_number, "page": num_page}
+    url = 'https://backend-challenge-summer-2018.herokuapp.com/challenges.json'
+    params = {'id': id_number, 'page': num_page}
     url_parts = list(urlparse.urlparse(url))  # parse the url in a list
     query = dict(urlparse.parse_qsl(url_parts[4]))
     query.update(params)  # add the params to the URL
@@ -30,12 +30,12 @@ def request_data():
         url = get_url(num_page, id_num)
         json_obj = urllib.urlopen(url)  # open a network object denoted by the url
         data = json.load(json_obj)  # convert data in json format
-        for each in (data["menus"]):  # Take data from URL
-            if "parent_id" in each:  # if the object has parent_id
-                data_dict[str(each["id"])] = [each["parent_id"], each["child_ids"]]
-            else:  # use "0" to difference between root_id and all the children
-                data_dict[str(each["id"])] = ['0', each["child_ids"]]
-            if data["pagination"]["total"] == each["id"]:  # Take total.pagination from each page as the limitation
+        for each in (data['menus']):  # Take data from URL
+            if 'parent_id' in each:  # if the object has parent_id
+                data_dict[str(each['id'])] = [each['parent_id'], each['child_ids']]
+            else:  # use '0' to difference between root_id and all the children
+                data_dict[str(each['id'])] = ['0', each['child_ids']]
+            if data['pagination']['total'] == each['id']:  # Take total.pagination from each page as the limitation
                 return data_dict
         num_page += 1
 
@@ -44,13 +44,13 @@ def request_data():
 
 
 def find_children(id_root):
-    data_branch.append(id_root)  # Add the id_root in data_branch position [0]
+    data_branch.append(int(id_root))  # Add the id_root in data_branch position [0]
     aux = []  # Auxiliary array to storage the children ids
     aux.extend(data_dict[id_root][1])  # Add the children of the id_root to aux array
     while len(aux) != 0:
         value = aux.pop(0)  # delete and save the first element of aux array in value
-        if data_dict[str(value)][1] is not []:  # check if id "value" has children
-            if value not in data_branch:  # check if id "value" is already in data_branch
+        if data_dict[str(value)][1] is not []:  # check if id 'value' has children
+            if value not in data_branch:  # check if id 'value' is already in data_branch
                 aux.extend(data_dict[str(value)][1])  # add children to aux array
         data_branch.append(value)  # add value to data_branch
     id_control.extend(data_branch)  # add data_branch to the id_control list
@@ -73,7 +73,7 @@ def find_id_root(data):
         if int(id) in id_control:  # to avoid repetition if an id as already in id_control
             continue  # continue with the next key
         id_parent = value[0]  # save value
-        while str(id_parent) != "0":  # Look for id_parent = 0 to get the id_root
+        while str(id_parent) != '0':  # Look for id_parent = 0 to get the id_root
             id = data[str(id)][0]
             id_parent = data[str(id)][0]  # id_parent breaks out of the loop
         data_branch = find_children(str(id))  # data_branch is a list with an id_root and all the id_root's children
@@ -84,19 +84,19 @@ def find_id_root(data):
 
 
 def output(data_branch):
-    values_dict = {"root_id": data_branch[0], "children": data_branch[1:]}
+    values_dict = {'root_id': data_branch[0], 'children': data_branch[1:]}
     if no_duplicates(data_branch):  # Look for duplicates in the values_dict
         valid_menu_array.append(values_dict)
-        output_dict["Valid_menus"] = valid_menu_array  # add valid_menus arrays to the output dictionary
+        output_dict['Valid_menus'] = valid_menu_array  # add valid_menus arrays to the output dictionary
     else:
         invalid_menu_array.append(values_dict)
-        output_dict["Invalid_menus"] = invalid_menu_array  # # add invalid_menus arrays to the output dictionary
+        output_dict['Invalid_menus'] = invalid_menu_array  # # add invalid_menus arrays to the output dictionary
 
 
 num_page = 1 # number of page, in the request_data function find the others valid pages
-id_num = 1  # possible inputs 1 or 2
+id_num = 2  # possible inputs 1 or 2
 id_control = []  # list of all the ids in the imput
-data_dict = {}  # dictionary with key = "id" and values value = "children"
+data_dict = {}  # dictionary with key = 'id' and values value = 'children'
 data_branch = []  # branch id list starting with the id_root to the children
 valid_menu_array = []  # list of the valid menu for the output
 invalid_menu_array = []  # list of the invalid menu for the output
